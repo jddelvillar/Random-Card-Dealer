849e41551c87a94719e018baabe5d28fbce17e2c
jddelvillar <delvillar2017.j@gmail.com>
2021-10-13 07:25:50 +0000

    Random Card Dealer

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+  document.querySelector(".card").classList.add(generateRandomSuit());
+};
+
+let generateRandomNumber = () => {
+  let Numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * Numbers.length);
+
+  return Numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let Suits = ["diams", "spades", "clubs", "hearts"];
+  let indexSuits = Math.floor(Math.random() * Suits.length);
+
+  return Suits[indexSuits];
 };
